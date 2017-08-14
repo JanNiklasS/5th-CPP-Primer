c7fff8b6ab6a6e006aadfd0e8253fe352fba7e98
JanNiklasS <jan_niklas.strugala@live.de>
2017-08-13 19:27:41 -0700

    Add files via upload

---
+++ prog2_4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+
+	unsigned u = 10;
+	unsigned u2 = 42;
+
+	std::cout << u2 - u << std::endl;	// 32
+	std::cout << u - u2 << std::endl;	// 4,294,967,264 
+
+	int i = 10;
+	int i2 = 42;
+
+	std::cout << i2 - i << std::endl;	// 32
+	std::cout << i - i2 << std::endl;	// -32
+	
+	std::cout << i - u << std::endl;	// 0
+	std::cout << u - i << std::endl;	// 0
+}
